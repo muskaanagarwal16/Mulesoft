8f4490412df04c02a70aef85ea6259b5733d1ed2
Muskaan Agarwal <103517985+muskaanagarwal16@users.noreply.github.com>
2022-04-13 01:28:20 +0530

    Add files via upload

---
+++ SQLITE1.sql
@@ -0,0 +1,4 @@
+-- SQLite
+SELECT name, actor, actress, year, director
+FROM MOVIE
+WHERE ACTOR = 'HRITHIK';
---
+++ SQLITE2.sql
@@ -0,0 +1,3 @@
+-- SQLite
+INSERT INTO MOVIE (name, actor, actress, year, director)
+VALUES ('BHARAT','SALMAN','KATRINA','2017','ALI,);
